54b5a752034c0e357e6e906730d36b6c95c881ec
abhishek <abhishekmazumder69@gmail.com>
2020-11-30 15:55:07 +0530

    products component created

--- src/App.js
+++ src/App.js
@@ -1,21 +1,37 @@
-
 // feature 1
 import React from 'react';
+import data from './data.json';
+import Products from './components/Products';
 
-function App() {
-  return (
-    <div className="grid-container">
-      <header>
-        <a href="/">React-Redux-Ecommerce</a>
-      </header>
-      <main>
-        Product List
-      </main>
-      <footer>
-        All rights is reserved
-      </footer>
-    </div>
-  );
+class App extends React.Component {
+	constructor() {
+		super();
+		this.state = {
+			products: data.products,
+			size: '',
+			sort: '',
+		};
+	}
+	render() {
+		return (
+			<div className="grid-container">
+				<header>
+					<a href="/">React-Redux-Ecommerce</a>
+				</header>
+				<main>
+          <div className="content">
+            <div className="main">
+              <Products products={this.state.products}/>
+            </div>
+            <div className="sidebar">
+              cart-item
+            </div>
+          </div>
+        </main>
+				<footer>All rights is reserved</footer>
+			</div>
+		);
+	}
 }
 
 export default App;
