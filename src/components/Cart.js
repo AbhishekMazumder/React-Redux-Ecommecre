8d4ba3c02f0bcaa502dfd848553cc7f02783d6ae
abhishek <abhishekmazumder69@gmail.com>
2020-12-10 18:02:29 +0530

    Redux Added to Filter Component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -43,7 +43,7 @@ export default class Cart extends Component {
 						<Fade left cascade>
 							<ul className="cart-items">
 								{cartItems.map(item => (
-									<li key={item.id}>
+									<li key={item._id}>
 										<div>
 											<img src={item.image} alt={item.tilte} />
 										</div>
